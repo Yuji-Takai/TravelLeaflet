9630e853d6224d60970d61ce8b2406855cde78e9
Jiawen Ge <jiawen_kevin_ge@gatech.edu>
2018-10-20 13:52:05 -0400

    Photo Storage Initial

--- TravelLeaflet/app/src/main/java/edu/gatech/travelleaflet/Models/Photo.java
+++ TravelLeaflet/app/src/main/java/edu/gatech/travelleaflet/Models/Photo.java
@@ -1,4 +1,33 @@
 package edu.gatech.travelleaflet.Models;
 public class Photo {
+    private String name;
+    private String url;
+    private static int count = 0;
 
+    public Photo() {}
+
+    public Photo(String name, String url) {
+        if (name.trim().equals("")) {
+            name = "NoName_" + count;
+            count++;
+        }
+        this.name = name;
+        this.url = url;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setUrl (String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
