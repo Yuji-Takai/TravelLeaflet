a8aa1f6eb620632e245e07710f51425092e882c4
Jiawen Ge <jiawen_kevin_ge@gatech.edu>
2018-10-20 23:00:35 -0400

    Checklist Functionality Done
    To do: pass in itemlist from trip.

--- TravelLeaflet/app/src/main/java/edu/gatech/travelleaflet/UserPages/LeafletActivities/ChecklistFrag.java
+++ TravelLeaflet/app/src/main/java/edu/gatech/travelleaflet/UserPages/LeafletActivities/ChecklistFrag.java
@@ -1,24 +1,28 @@
 package edu.gatech.travelleaflet.UserPages.LeafletActivities;
 
-import android.content.ClipData;
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import edu.gatech.travelleaflet.Models.Item;
-import edu.gatech.travelleaflet.Models.Trip;
 import edu.gatech.travelleaflet.R;
 
-public class ChecklistFrag extends Fragment {
+public class ChecklistFrag extends Fragment implements AddItem.OnItemListener{
+
+    @Override
+    public void sendItem(Item i) {
+        checklist.add(i);
+    }
+
+    private Button addItem;
+    private List<Item> checklist;
 
     public ChecklistFrag() {
         // Required empty public constructor
@@ -39,10 +43,19 @@ public class ChecklistFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_checklist, container, false);
-        List<Item> checklist = ;
+        checklist = new ArrayList<Item>();
+
+        addItem = view.findViewById(R.id.addItem);
+        addItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddItem dialog = new AddItem();
+                dialog.show(getFragmentManager(), "Add Item");
+            }
+        });
 
         ListView listView = (ListView) view.findViewById(R.id.checklist_listview);
-        ItemAdapter listViewAdapter = new ItemAdapter(checklist ,getActivity());
+        ItemAdapter listViewAdapter = new ItemAdapter(checklist, getActivity());
 
         listView.setAdapter(listViewAdapter);
         // Inflate the layout for this fragment
