3ed00d09d1ff7cca8d8f1703a6a2bc8b574d3e52
carpediem98 <jiachunjun2013@gmail.com>
2018-10-21 00:33:01 -0400

    commit

--- TravelLeaflet/app/src/main/java/edu/gatech/travelleaflet/UserPages/MainActivities/DashboardFrag.java
+++ TravelLeaflet/app/src/main/java/edu/gatech/travelleaflet/UserPages/MainActivities/DashboardFrag.java
@@ -2,13 +2,25 @@ package edu.gatech.travelleaflet.UserPages.MainActivities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewDebug;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import edu.gatech.travelleaflet.Models.Trip;
 import edu.gatech.travelleaflet.Models.User;
 import edu.gatech.travelleaflet.R;
 import edu.gatech.travelleaflet.UserPages.LeafletActivities.LeafletMainActivity;
@@ -18,6 +30,12 @@ public class DashboardFrag extends Fragment {
     public DashboardFrag() {
         // Required empty public constructor
     }
+
+    ListView current_trips;
+    FirebaseDatabase database;
+    DatabaseReference ref;
+    ArrayList<Trip> list;
+    ArrayAdapter<Trip> adapter;
 
     public static DashboardFrag newInstance() {
         DashboardFrag fragment = new DashboardFrag();
@@ -29,7 +47,6 @@ public class DashboardFrag extends Fragment {
         super.onCreate(savedInstanceState);
 
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -44,9 +61,32 @@ public class DashboardFrag extends Fragment {
             }
         });
 
+        current_trips = (ListView) view.findViewById(R.id.current_trips);
+        database = FirebaseDatabase.getInstance();
+        ref = database.getReference("user");
+        list = new ArrayList<>();
+        adapter = new ArrayAdapter<Trip>(this, R.layout.trip_info, R.id.trip_info, list);
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds: dataSnapshot.getChildren()) {
+                    User user = ds.getValue(User.class);
+                    list = user.getTrips();
+                }
+                current_trips.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
 
         return view;
     }
+
+
 
 
 }
